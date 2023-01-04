082d50c68494480a5193f8a3a3b1faff5227bcce
백대홍 <bdh3620@naver.com>
2023-01-04 23:20:41 +0900

    edit

--- RichPaper1/RichPaper/Onboarding.swift
+++ RichPaper1/RichPaper/Onboarding.swift
@@ -17,17 +17,19 @@ struct Onboarding: View {
             Color(.systemGroupedBackground)
                 .ignoresSafeArea(.all)
             
-            
+                
             
             
             VStack(alignment: .center) {
                 Text("Rich Paper")          //제일 위 제목
                     .font(.largeTitle.bold())
                     .foregroundColor(.black)
+                    .padding(.top, 20)
+                
+                
                 Spacer()
-                
-                
                 VStack(alignment: .leading, spacing: 20) {                   //VStack으로 나열
+                    
                     HStack(alignment: .center) {           // 이미지랑 텍스트 같이 나열 1
                         
                         Image(systemName: "pencil.and.outline")
@@ -38,10 +40,10 @@ struct Onboarding: View {
                         
                         Text("Take out your paper right now and write down your goals in the time you want!")
                             .font(.headline)
-                        
+                            .padding([.leading, .trailing, .bottom])
                     }
                     
-                    HStack(alignment: .center, spacing: 0) {            //2
+                    HStack(alignment: .center) {            //2
                         
                         Image(systemName: "calendar")
                             .foregroundColor(.accentColor)
@@ -51,7 +53,7 @@ struct Onboarding: View {
                         
                         Text("Set your own plans for Period and see how you change.")
                             .font(.headline)
-                        
+                            .padding([.leading, .trailing, .bottom])
                         
                     }
                     HStack(alignment: .center) {                //3
@@ -64,9 +66,11 @@ struct Onboarding: View {
                         
                         Text("If you could achieve your desired goal in 24 hours, which one would you choose?")
                             .font(.headline)
+                            .padding([.leading, .trailing, .bottom])
                         
                         
                     }
+                    
                     HStack(alignment: .center) {                    //4
                         
                         Image(systemName: "figure.run")
@@ -77,12 +81,15 @@ struct Onboarding: View {
                         
                         Text("If you want to find yourself working every day toward your goal, start now.")
                             .font(.headline)
-                        
+                            .padding([.leading, .trailing, .bottom])
                         
                     }
                     
-                }
-                Spacer()
+                } .padding()
+                    Spacer()
+            
+                
+                
                 if showsDismissButton {
                     // 온보딩이 끝나고 메인 페이지로 넘어가기 위한 버튼
                     // ShowOnboarding 값을 false로 바꾸기 때문에 최초 실행 이후에는 onboardingview를 보여주지 않는다.
@@ -91,14 +98,14 @@ struct Onboarding: View {
                             ShowOnboarding.toggle()
                         }, label: {
                             Text("Get Start")
+                                .padding()
                                 .foregroundColor(.white)
                                 .bold()
-                                .cornerRadius(10)
-                                .frame(width: 200, height: 50)
-                                .background(Color(red: 60 / 255, green: 179 / 255, blue: 113 / 255))
+                                .cornerRadius(30)
+                                .background(Color(.systemBlue))
+                                
                         })
                     }
-                Spacer()
                 Spacer()
             }
            
